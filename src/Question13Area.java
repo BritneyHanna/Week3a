7e559edc11ab64e8849e94e69abc212125adc6fc
Britney Hanna <athina@Britneys-MacBook-Air.local>
2020-09-05 15:24:04 -0400

    Added question 13

---
+++ src/Question13Area.java
@@ -0,0 +1,14 @@
+
+public class Question13Area {
+
+	public static void main(String[] args) {
+
+		System.out.println(arearectangle(6, 7));
+		System.out.println(arearectangle(1500, 7));
+
+	}
+
+	public static int arearectangle(int length, int width) {
+		return length * width;
+	}
+}
